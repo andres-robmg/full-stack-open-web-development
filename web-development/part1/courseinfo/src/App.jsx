f3f45a2bdb4f5166a67bf716ee3ce43f5868981b
andres-robmg <andresrobmg13@gmail.com>
2025-09-03 09:30:00 -0500

    1.2 courseinfo

--- web-development/part1/courseinfo/src/App.jsx
+++ web-development/part1/courseinfo/src/App.jsx
@@ -7,9 +7,24 @@ const Header = ({ course }) => {
 }
 
 const Content = ({ parts }) => {
+  // It will render the 3 <Part /> components, each one with their respective name and number of exercises 
+  /* return (
+    { <div>
+      {parts.map(part => (<Part name={part.name} exercises={part.exercises} />))}
+    </div> } */
   return (
     <div>
-      {parts.map(part => (<p>{part.name} {part.exercises}</p>))}
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+}
+
+const Part = ({ name, exercises }) => {
+  return (
+    <div>
+      <p>{name} {exercises}</p>
     </div>
   )
 }
