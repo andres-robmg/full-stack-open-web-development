927d82c1c90a85ee59e87f0c6a6e96517a5a2f63
andres-robmg <andresrobmg13@gmail.com>
2025-09-05 13:11:40 -0500

    1.3 refactoring variables (3 object for each part)

--- web-development/part1/courseinfo/src/App.jsx
+++ web-development/part1/courseinfo/src/App.jsx
@@ -40,20 +40,30 @@ const Total = ({ exercises }) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
+  // Part 1.3 Refactoring: Variables/ Objects definition
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  // Refactoring: Objects in array (same as 1.4 requirement but with other approach)
   const parts = [
-    { name: part1, exercises: exercises1 },
-    { name: part2, exercises: exercises2 },
-    { name: part3, exercises: exercises3 },
+    part1,
+    part2,
+    part3,
   ]
 
-  const totalExercises = exercises1 + exercises2 + exercises3
+  const totalExercises = part1.exercises + part2.exercises + part3.exercises
+
 
   return (
     <div>
